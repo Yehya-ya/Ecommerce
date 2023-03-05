888a00d1b0c172de2160dc8d9ae67634b4f75822
Yehya-ya <yahya.yaser@scantitan.com>
2023-03-05 17:28:38 +0400

    adding telescope to the project

--- app/Services/PriceService.php
+++ app/Services/PriceService.php
@@ -9,7 +9,6 @@ class PriceService
 {
     public function getPrice(int $amount, string $from, string $to): float
     {
-        Log::warning([$from, $to]);
         $currency_rate = CurrencyRate::where('from', $from)->where('to', $to)->first();
         if (! $currency_rate) {
             return $amount;
