e66939e348e769d7963cc9ee81543d1dcebe25b1
Yehya-ya <yahya.yaser@scantitan.com>
2024-04-21 19:23:25 +0400

    adding ico

--- resources/views/layouts/app_layout.blade.php
+++ resources/views/layouts/app_layout.blade.php
@@ -29,7 +29,7 @@
     <link rel="stylesheet" href="{{ asset('assets/vendors/perfect-scrollbar/perfect-scrollbar.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.svg') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('favicon.svg') }}" type="image/x-icon">
 
 
     <!-- Page Styles -->
